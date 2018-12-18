5a7d3b1e163c99a43735e485332bcc251e2417a0
Daniel Wagner <me@dmwit.com>
2018-12-18 10:27:11 -0500

    actually use the parser stuff

--- bokrifulse.lua
+++ bokrifulse.lua
@@ -1,188 +1,3 @@
--- ff: empty
--- 4x: top half
--- 5x: bottom half
--- 6x: left half
--- 7x: right half
--- 8x: disconnected half
--- dx: virus
--- x0: yellow
--- x1: red
--- x2: blue
--- 10-23: letters
-local new_board =
-	{ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xd0, 0xd0, 0xd1, 0xd1, 0xd0, 0xd0, 0xd2, 0xd2
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-	}
--- TODO: 0x99 is a funnier default =)
-local new_bcd_virus_count = 0x08
--- TODO: work out how the counts for individual colors of virus are stored, and
--- fix them up, too, maybe
--- 0: yellow
--- 1: red
--- 2: blue
-local new_pill_left  = 0
-local new_pill_right = 1
-local new_pill = 3 * new_pill_left + new_pill_right
-local new_speed_adjustment = 0
--- left-most column is 0, right-most column is 7
-local goal_pill_x = 6
--- bottom row is 0, top row is 15
-local goal_pill_y = 4
--- 0: unrotated
--- 1: counterclockwise
--- 2: mirrored
--- 3: clockwise
-local goal_pill_orientations = { 3 }
-
-local initial_state = 0 -- waiting for the user to start the game
-local virus_generation_state = 1 -- the game is picking random viruses, let's politely wait for it to finish
-local ready_state = 2 -- our turn: time modify the board and pill lookahead however we want
-local awaiting_control_state = 3 -- we have set up the board and pill lookahead appropriately, now we're waiting for the pill animation to complete and the pill to appear at the top of the board
-local under_control_state = 4 -- the player is doing maneuvers
-local maybe_maybe_locked_state = 5 -- the pill is about to drop one row
-local maybe_locked_state = 6 -- the pill just dropped one row, and may be either locked about to drop another row
-local locked_state = 7 -- the pill locked two frames ago
-local CURRENT_STATE = initial_state
-local MANEUVER_START_FRAME
-
-local function to_bcd(n)
-	return n % 10 + 8 * (n - n % 10) / 5
-end
-
-local function from_bcd(n)
-	return n % 16 + 5 * (n - n % 16) / 8
-end
-
-local function initial_action()
-	local mode = memory.readbyte(0x46)
-	local player_count = memory.readbyte(0x727)
-	local level = math.min(memory.readbyte(0x316), 20)
-	local bcd_virus_count = memory.readbyte(0x324)
-	local max_virus_count = (level+1)*4
-	-- virus_count < max_virus_count is important: we want to wait until
-	-- they're equal, but sometimes they are spuriously equal when the number
-	-- of viruses is left over unchanged from a previous game
-	if mode == 8 and player_count == 1 and bcd_virus_count < to_bcd(max_virus_count) then
-		CURRENT_STATE = virus_generation_state
-	end
-end
-
-local function virus_generation_action()
-	local level = math.min(memory.readbyte(0x316), 20)
-	local bcd_virus_count = memory.readbyte(0x324)
-	local max_virus_count = (level+1)*4
-	if bcd_virus_count == to_bcd(max_virus_count) then
-		CURRENT_STATE = ready_state
-	end
-end
-
-local function ready_action()
-	for i, v in pairs(new_board) do
-		memory.writebyte(0x400 + i - 1, v)
-	end
-	memory.writebyte(0x324, new_bcd_virus_count)
-	for i = 0, 127 do
-		memory.writebyte(0x780 + i, new_pill)
-	end
-	memory.writebyte(0x31a, new_pill_left)
-	memory.writebyte(0x31b, new_pill_right)
-	memory.writebyte(0x30a, new_speed_adjustment)
-	CURRENT_STATE = awaiting_control_state
-end
-
-local function awaiting_control_action()
-	local drop = memory.readbyte(0x312)
-	if drop > 0 then
-		MANEUVER_START_FRAME = emu.framecount()
-		CURRENT_STATE = under_control_state
-	end
-end
-
-local function under_control_action()
-	local drop = memory.readbyte(0x312)
-	if drop == 0 then
-		CURRENT_STATE = maybe_maybe_locked_state
-	end
-end
-
-local function maybe_maybe_locked_action()
-	local drop = memory.readbyte(0x312)
-	if drop == 0 then
-		CURRENT_STATE = maybe_locked_state
-	else
-		CURRENT_STATE = under_control_state
-	end
-end
-
-local function maybe_locked_action()
-	local drop = memory.readbyte(0x312)
-	if drop == 0 then
-		CURRENT_STATE = locked_state
-	else
-		CURRENT_STATE = under_control_state
-	end
-end
-
-local function locked_action()
-	local pill_x = memory.readbyte(0x305)
-	local pill_y = memory.readbyte(0x306)
-	local pill_orientation = memory.readbyte(0x325)
-	-- This is not quite right; it could be off by one frame if a pill
-	-- naturally drops one row, then gets forced to drop a row by the player on
-	-- the very next frame, and the forced drop locks it. Currently, since we
-	-- set the drop counter bonus index to 0 (and so natural drops always
-	-- happen after an even number of frames), this can only occur if all of
-	-- the following happen:
-	--
-	-- * The pill comes under player control on an odd frame.
-	-- * The player lets it drop naturally until the second-to-last row before
-	--   it locks.
-	-- * On the exact frame that the pill transitions from the second-to-last
-	--   row to the last row, the player presses down.
-	--
-	-- This combination seems very uncommon to me, especially given that I
-	-- expect the way most people will use this tool is to hold down as much as
-	-- they can. So we'll just report a one-frame-incorrect answer in the
-	-- vanishingly unlikely case that all the conditions above are met.
-	local maneuver_frames = emu.framecount() - 3 - MANEUVER_START_FRAME + 1
-
-	local SUCCESS = false
-	for _, goal_pill_orientation in pairs(goal_pill_orientations) do
-		SUCCESS = SUCCESS or pill_orientation == goal_pill_orientation
-	end
-	SUCCESS = SUCCESS and pill_x == goal_pill_x and pill_y == goal_pill_y
-
-	print('maneuver ' .. (SUCCESS and 'completed' or 'failed') .. ' in ' .. maneuver_frames .. ' frames')
-	CURRENT_STATE = ready_state
-end
-
-local actions =
-	{ [initial_state] = initial_action
-	, [virus_generation_state] = virus_generation_action
-	, [ready_state] = ready_action
-	, [awaiting_control_state] = awaiting_control_action
-	, [under_control_state] = under_control_action
-	, [maybe_maybe_locked_state] = maybe_maybe_locked_action
-	, [maybe_locked_state] = maybe_locked_action
-	, [locked_state] = locked_action
-	}
-
-emu.registerbefore(function() actions[CURRENT_STATE]() end)
-
 local function get_prefix()
 	local dir_sep = package.config:sub(1,1)
 	local script_dir = debug.getinfo(2, "S").source:match('.(.*' .. dir_sep .. ')')
@@ -196,15 +11,34 @@ end
 local prefix = get_prefix()
 local config_filename = prefix .. 'config'
 local default_problem =
-	{ board = new_board -- TODO: inline all of these
-	, bcd_virus_count = new_bcd_virus_count
-	, pill = new_pill
-	, speed_adjustment = new_speed_adjustment
-	, goal_x = goal_pill_x
-	, goal_y = goal_pill_y
-	, goal_orientations = goal_pill_orientations
+	{ board =
+		{ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0x60, 0x70, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xd0, 0xd0, 0xff, 0xff, 0xff
+		, 0xff, 0xff, 0xff, 0xd0, 0xd0, 0xff, 0xff, 0xff
+		}
+	, bcd_virus_count = 0x99
+	, pill_left = 0
+	, pill_right = 0
+	, speed_adjustment = 0
+	, goal_x = 3
+	, goal_y = 3
+	, goal_orientations = { 0, 2 }
 	}
-local PROBLEMS
+local PROBLEMS = { default_problem }
+local CURRENT_PROBLEM = 1
 
 -- Where you would write
 --     x <- act
@@ -229,6 +63,14 @@ local function forM(kvs, f)
 		if fv[1] then RESULT[k] = fv[2] else return fv end
 	end
 	return {true, RESULT}
+end
+
+local function to_bcd(n)
+	return n % 10 + 8 * (n - n % 10) / 5
+end
+
+local function from_bcd(n)
+	return n % 16 + 5 * (n - n % 16) / 8
 end
 
 local function parse_file(filename)
@@ -337,7 +179,7 @@ local function parse_pill(vs)
 	return bind({#vs == 1, ('Expected exactly one pill (saw %d)'):format(#vs)}, function()
 	local v = vs[1]
 	return bind({v:find('^[rRyYbB][rRyYbB]$'), ('Expected two colors drawn from r, y, b (saw %s)'):format(v)}, function()
-	return {true, 3*color_lower_nibble[v:sub(1,1)] + color_lower_nibble[v:sub(2,2)]}
+	return {true, {color_lower_nibble[v:sub(1,1)], color_lower_nibble[v:sub(2,2)]}}
 	end) end)
 end
 
@@ -373,7 +215,8 @@ local function parse_problem(filename)
 	return {true,
 		{ board = concat(rows)
 		, bcd_virus_count = to_bcd(virus_count)
-		, pill = pill
+		, pill_left = pill[1]
+		, pill_right = pill[2]
 		, speed_adjustment = speed_adjustment
 		, goal_x = goal_x
 		, goal_y = goal_y
@@ -416,4 +259,132 @@ local function reload_config()
 	end
 end
 
+local initial_state = 0 -- waiting for the user to start the game
+local virus_generation_state = 1 -- the game is picking random viruses, let's politely wait for it to finish
+local ready_state = 2 -- our turn: time modify the board and pill lookahead however we want
+local awaiting_control_state = 3 -- we have set up the board and pill lookahead appropriately, now we're waiting for the pill animation to complete and the pill to appear at the top of the board
+local under_control_state = 4 -- the player is doing maneuvers
+local maybe_maybe_locked_state = 5 -- the pill is about to drop one row
+local maybe_locked_state = 6 -- the pill just dropped one row, and may be either locked about to drop another row
+local locked_state = 7 -- the pill locked two frames ago
+local CURRENT_STATE = initial_state
+local MANEUVER_START_FRAME
+
+local function initial_action()
+	local mode = memory.readbyte(0x46)
+	local player_count = memory.readbyte(0x727)
+	local level = math.min(memory.readbyte(0x316), 20)
+	local bcd_virus_count = memory.readbyte(0x324)
+	local max_virus_count = (level+1)*4
+	-- virus_count < max_virus_count is important: we want to wait until
+	-- they're equal, but sometimes they are spuriously equal when the number
+	-- of viruses is left over unchanged from a previous game
+	if mode == 8 and player_count == 1 and bcd_virus_count < to_bcd(max_virus_count) then
+		CURRENT_STATE = virus_generation_state
+	end
+end
+
+local function virus_generation_action()
+	local level = math.min(memory.readbyte(0x316), 20)
+	local bcd_virus_count = memory.readbyte(0x324)
+	local max_virus_count = (level+1)*4
+	if bcd_virus_count == to_bcd(max_virus_count) then
+		CURRENT_STATE = ready_state
+	end
+end
+
+local function ready_action()
+	local problem = PROBLEMS[CURRENT_PROBLEM]
+	for i, v in pairs(problem.board) do
+		memory.writebyte(0x400 + i - 1, v)
+	end
+	memory.writebyte(0x324, problem.bcd_virus_count)
+	for i = 0, 127 do
+		memory.writebyte(0x780 + i, 3*problem.pill_left + problem.pill_right)
+	end
+	memory.writebyte(0x31a, problem.pill_left)
+	memory.writebyte(0x31b, problem.pill_right)
+	memory.writebyte(0x30a, problem.speed_adjustment)
+	CURRENT_STATE = awaiting_control_state
+end
+
+local function awaiting_control_action()
+	local drop = memory.readbyte(0x312)
+	if drop > 0 then
+		MANEUVER_START_FRAME = emu.framecount()
+		CURRENT_STATE = under_control_state
+	end
+end
+
+local function under_control_action()
+	local drop = memory.readbyte(0x312)
+	if drop == 0 then
+		CURRENT_STATE = maybe_maybe_locked_state
+	end
+end
+
+local function maybe_maybe_locked_action()
+	local drop = memory.readbyte(0x312)
+	if drop == 0 then
+		CURRENT_STATE = maybe_locked_state
+	else
+		CURRENT_STATE = under_control_state
+	end
+end
+
+local function maybe_locked_action()
+	local drop = memory.readbyte(0x312)
+	if drop == 0 then
+		CURRENT_STATE = locked_state
+	else
+		CURRENT_STATE = under_control_state
+	end
+end
+
+local function locked_action()
+	local problem = PROBLEMS[CURRENT_PROBLEM]
+	local pill_x = memory.readbyte(0x305)
+	local pill_y = memory.readbyte(0x306)
+	local pill_orientation = memory.readbyte(0x325)
+	-- This is not quite right; it could be off by one frame if a pill
+	-- naturally drops one row, then gets forced to drop a row by the player on
+	-- the very next frame, and the forced drop locks it. Currently, since we
+	-- set the drop counter bonus index to 0 (and so natural drops always
+	-- happen after an even number of frames), this can only occur if all of
+	-- the following happen:
+	--
+	-- * The pill comes under player control on an odd frame.
+	-- * The player lets it drop naturally until the second-to-last row before
+	--   it locks.
+	-- * On the exact frame that the pill transitions from the second-to-last
+	--   row to the last row, the player presses down.
+	--
+	-- This combination seems very uncommon to me, especially given that I
+	-- expect the way most people will use this tool is to hold down as much as
+	-- they can. So we'll just report a one-frame-incorrect answer in the
+	-- vanishingly unlikely case that all the conditions above are met.
+	local maneuver_frames = emu.framecount() - 3 - MANEUVER_START_FRAME + 1
+
+	local SUCCESS = false
+	for _, goal_orientation in pairs(problem.goal_orientations) do
+		SUCCESS = SUCCESS or pill_orientation == goal_orientation
+	end
+	SUCCESS = SUCCESS and pill_x == problem.goal_x and pill_y == problem.goal_y
+
+	print('maneuver ' .. (SUCCESS and 'completed' or 'failed') .. ' in ' .. maneuver_frames .. ' frames')
+	CURRENT_STATE = ready_state
+end
+
+local actions =
+	{ [initial_state] = initial_action
+	, [virus_generation_state] = virus_generation_action
+	, [ready_state] = ready_action
+	, [awaiting_control_state] = awaiting_control_action
+	, [under_control_state] = under_control_action
+	, [maybe_maybe_locked_state] = maybe_maybe_locked_action
+	, [maybe_locked_state] = maybe_locked_action
+	, [locked_state] = locked_action
+	}
+
 reload_config()
+emu.registerbefore(function() actions[CURRENT_STATE]() end)
